fbca5b3ed5056845b99ebdebe3881fa9c358249f
Tom Barron <tom.barron@comcast.net>
2016-01-01 14:04:40 -0500

    Add tests and ignores

---
+++ tests/test_spackle.py
@@ -0,0 +1,44 @@
+import glob
+import os
+import pdb
+import spackle
+
+def test_link_linkpath_with_srcdir():
+    """
+    spackle link foo --from /tmp
+    """
+    assert spackle.linkpath('/tmp', 'foo') == '/tmp/foo'
+
+
+def test_link_linkpath_wo_srcdir():
+    """
+    spackle link bar
+    """
+    home = os.getenv('HOME')
+    assert spackle.linkpath(None, 'bar') == '{}/bin/bar'.format(home)
+
+    
+def test_link_payload_with_path():
+    """
+    spackle link git --to /usr/local/bin/tclsh
+    """
+    assert spackle.payload('/usr/local/bin/tclsh', 'git') == '/usr/local/bin/tclsh'
+
+
+def test_link_payload_wo_path():
+    """
+    spackle link tmux
+    """
+    spack_path = os.path.join(os.getenv('HOME'),
+                              'prj',
+                              'spack',
+                              'opt',
+                              'spack',
+                              'darwin-x86_64',
+                              'gcc-4.2.1',
+                              )
+    tmux_dir = glob.glob('{}/tmux-*'.format(spack_path))
+    exp = '{}/bin/tmux'.format(tmux_dir.pop())
+    assert spackle.payload(None, 'tmux') == exp
+
+    
